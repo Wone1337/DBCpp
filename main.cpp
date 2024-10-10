21e5a331947eab288a4bc0448c34da6e1d6100e7
Wone1337 <stepan.moiseev.2018@bk.ru>
2024-10-11 06:59:50 +0800

    super_easy_mem_alloc

--- main.cpp
+++ main.cpp
@@ -1,103 +1,17 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
-#include <sys/mman.h>
 #include <memory>
 #include <thread>
 #include <unistd.h>
-
-namespace biba
-    {
-        template<typename T>class Allocator;
-        template<typename T> class MemoryManager;
-
-        template<typename T> class Allocator
-        {
-            public:
-
-            void* biba_init()
-            {
-                
-                   to_rotate = sbrk(size);
-                   return to_rotate;           
-            }
-
-            void* biba()
-            {
-
-            }
-
-            void* freedom()
-            {
-
-
-            } 
-
-            private:
-            size_t size = sizeof(T);
-            void *to_rotate;
-        };
-
-
-        template<typename T> class MemoryManager:public Allocator<T>
-            {
-                public:
-                MemoryManager() = default;
-                explicit MemoryManager(const T& _variable):InType(_variable) 
-                {
-                    Allocator<T>::biba_init();
-                }
-                explicit MemoryManager(const T&& _variable):InType(std::move(_variable)){}
-                MemoryManager &operator=(MemoryManager&& value) noexcept = default;
-                MemoryManager &operator=(const MemoryManager& value) noexcept = default;
-                
-                
-                ~MemoryManager(){};
-                
-
-                private:
-                T InType;
-
-            };
-
-       
-           
-
-    
-
-    };
-//MemoryManager<int[]> mem ;
-
-
-void* my_malloc(size_t size) {
-    return sbrk(size); // Запрашиваем у sbrk `size` байт
-}
-
-
+#include <limits>
+#include <cmath>
+#include "main.hpp"
 
 int main([[maybe_unused]]int argc,[[maybe_unused]] char **argv) 
 {
-    
-    int s = 10;
-// const int &f  = s;
-// biba::MemoryManager<int> e{10};
 
- int* array = (int*)my_malloc(10 * sizeof(int));
-
-
-// biba::MemoryManager<int> f(s);
-
-
-// std::cout << sizeof(intptr_t); 
 
 return 0;
 }
-
-
-//  Указатели и память:
-//    Реализуй класс MemoryManager, который управляет динамически выделенной памятью. Класс должен предоставлять методы для:
-//        Выделения и освобождения памяти (аналог new и delete). (использование mmap)
-//        Проверки, выделена ли память. (check_mem)
-//        Подсчета текущего объема выделенной памяти.(capacity)
-
-
--- main.hpp
+++ main.hpp
@@ -1,8 +1,118 @@
-#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unistd.h>
+#include <cmath>
 
-class ge;
 
-class gf
-{
-   ge *C;
-};
+#define MAIN_HPP
+#ifdef MAIN_HPP
+
+namespace biba
+    {
+        template<typename T>class Allocator;
+        template<typename T> class MemoryManager;
+
+        template<typename T> class Allocator
+        {
+            public:
+            void* biba() noexcept
+            {
+                to_rotate = sbrk(size);
+                
+                if(to_rotate == reinterpret_cast<void*>(-1))
+                {
+                    std::cerr << "Error\n" << std::flush;
+                    to_rotate = nullptr;
+                    return nullptr;
+                }
+
+                capacity += size;
+                return to_rotate;
+            }
+
+            void* freedom() noexcept
+            {
+              
+                    to_rotate = sbrk(-size);
+                    to_rotate = nullptr;
+                    capacity -= size;
+                
+                return to_rotate;
+            } 
+            
+            private:
+            const static size_t size = sizeof(T);
+
+            protected:
+            static inline size_t capacity {};
+            void *to_rotate; 
+        };
+
+
+        template<typename T> class MemoryManager:protected Allocator<T>
+            {
+                public:
+                MemoryManager()
+                {
+                     Allocator<T>::biba();
+                }
+                explicit MemoryManager(const T& _variable):InType(_variable) 
+                {
+                     Allocator<T>::biba();
+                }
+                explicit MemoryManager(const T&& _variable):InType(std::move(_variable))
+                {
+                     Allocator<T>::biba();
+                }
+                MemoryManager &operator=(MemoryManager&& value) noexcept = default;
+                MemoryManager &operator=(const MemoryManager& value) noexcept = default;
+
+                MemoryManager &operator=(T&& value) noexcept 
+                {
+                    InType = value;
+            
+                    return *this;
+                }
+                MemoryManager &operator=(T& value) noexcept 
+                {
+                    InType = value;
+            
+                    return *this;
+                }
+
+                ~MemoryManager()
+                {
+                    Allocator<T>::freedom();
+                };
+                
+
+                void Capasity()
+                {
+                    std::cout << "Capacity of memory alloc: " << Allocator<T>::capacity << " MB\n" << std::flush;
+                }
+
+                void Mem_checker()
+                {
+                     if(Allocator<T>::to_rotate == nullptr)
+                     {
+                        std::cout << "No one object with allocated memory is not available\n" << std::flush;
+                     }
+                     else
+                     {
+                        std::cout << "Memory allocate now!" << '\n' <<  std::flush;
+                     }
+                }
+
+                private:
+                T InType;
+
+            };
+
+       
+           
+
+
+    };
+
+#endif
